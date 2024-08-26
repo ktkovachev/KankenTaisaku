a093d8b472ed0046557aab91bb620d28460bdafd
Kiril Kovachev <ktkovachev@gmail.com>
2024-08-26 21:23:50 +0100

    Create skeleton for generator program (CLI)

---
+++ kanken_processor.py
@@ -0,0 +1,33 @@
+import sys
+import argparse
+from kanjipedia_collator import parse_all_kanji
+from anki_deck_generator import build_deck
+
+def generate_anki_deck():
+    print("Building Anki deck...", file=sys.stderr)
+    pass
+
+def generate_data_files():
+    all_kanji = parse_all_kanji()
+    with open("build/kanji.tsv", mode="w") as f:
+        f.write("\n".join(str(kanji) for kanji in all_kanji))
+
+def main():
+    cli_parser = argparse.ArgumentParser(
+        prog="kanken-processor",
+        description="Program that collates Kanken data",
+    )
+    cli_parser.add_argument("action", choices=["compile-data", "compile-deck"])
+
+    args = cli_parser.parse_args()
+
+    action: str = args.action
+    if action == "compile-data":
+        generate_data_files()
+    elif action == "compile-deck":
+        generate_anki_deck()
+    else:
+        print("Invalid action:", action, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
