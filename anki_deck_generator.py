e65c76457315c8ce183b70d2a98a88fd34630e6e
Kiril Kovachev <ktkovachev@gmail.com>
2024-08-30 23:57:38 +0100

    Complete (MVP) Anki deck generation

---
+++ anki_deck_generator.py
@@ -0,0 +1,137 @@
+from typing import Iterable
+import genanki
+import os.path
+
+from data_models import Kanji, Kotoba
+
+KANJI_MODEL_ID = 1976405439
+KOTOBA_MODEL_ID = 1616488250
+
+# NOTE: May use part-of-speech-differentiated card variants in the future.
+# VERB_MODEL_ID = 1844777987
+# ADJECTIVE_MODEL_ID = 1118383897
+# YOJIJUKUGO_MODEL_ID = 1116935715
+# INDECLINABLE_MODEL_ID = 1753028315
+
+KANKEN_DECK_ID = 1169437805
+KANKEN_KANJI_SUBDECK_ID = 1228308188
+KANKEN_KOTOBA_SUBDECK_ID = 1095745966
+
+def load_generic(file_path: str):
+    with open(file_path) as f:
+        return f.read()
+
+def load_template(template_name: str):
+    return load_generic(os.path.join("anki", "templates", template_name))
+
+def load_style(template_name: str):
+    return load_generic(os.path.join("anki", "styles", template_name))
+
+
+KANJI_MODEL = genanki.Model(
+    KANJI_MODEL_ID,
+    "Kanken Kanji",
+    fields=[
+        {"name": "Kanji"},
+        {"name": "Level"},
+        {"name": "Is kokuji?"},
+        {"name": "Meanings"},
+        {"name": "Unclassified on"},
+        {"name": "Go-on"},
+        {"name": "Kan-on"},
+        {"name": "Kan'yō-on"},
+        {"name": "Tō-on"},
+        {"name": "Sō-on"},
+        {"name": "Kun"},
+        # {"name": "Uetsuki"},
+        # {"name": "Shitatsuki"},
+        {"name": "Radical"},
+        {"name": "Stroke count"},
+        {"name": "Radical-added stroke count"},
+        # {"name": "JIS"},
+        {"name": "Glyph origin"},
+        # {"name": "Replaces"},
+        # {"name": "Replaced by"},
+        # {"name": "Simplified forms"},
+        # {"name": "Traditional forms"},
+    ],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": load_template("kanji_front.html"),
+            "afmt": load_template("kanji_back.html")
+        }
+    ],
+    css=load_style("kanji.css")
+)
+
+KOTOBA_MODEL = genanki.Model(
+    KOTOBA_MODEL_ID,
+    "Kanken Kotoba",
+        fields=[
+            {"name": "Word"},
+            {"name": "Reading"},
+            {"name": "Pitch accents pattern(s)"},
+            {"name": "Meanings"},
+            {"name": "Is jukujikun/ateji?"},
+        ],
+        templates=[
+            {
+                "name": "Card 1",
+                "qfmt": load_template("kanji_front.html"),
+                "afmt": load_template("kanji_back.html")
+            }
+        ],
+        css=load_style("kotoba.css")
+)
+
+
+class KanjiNote(genanki.Note):
+    @property
+    def guid(self):
+        return genanki.guid_for(self.fields[0]) # The character field only
+
+class KotobaNote(genanki.Note):
+    @property
+    def guid(self):
+        return genanki.guid_for(self.fields[0]) # The word field only
+
+def create_kanji_note(kanji: Kanji) -> KanjiNote:
+    return KanjiNote(
+        KANJI_MODEL,
+        fields=kanji.as_tuple()
+    )
+
+def create_kotoba_note(kotoba: Kotoba) -> KotobaNote:
+    return KotobaNote(
+        KOTOBA_MODEL,
+        fields=kotoba.as_tuple()
+    )
+
+def build_deck(kanjis: Iterable[Kanji], kotobas: Iterable[Kotoba]):
+    # Currently appears to be unnecessary, as cards are only sorted into the subdecks, not the main deck
+    kanken_deck = genanki.Deck(
+        KANKEN_DECK_ID,
+        "漢検一級"
+    )
+
+    kanken_kanji_subdeck = genanki.Deck(
+        KANKEN_KANJI_SUBDECK_ID,
+        "漢検一級::漢字"
+    )
+
+    kanken_kotoba_subdeck = genanki.Deck(
+        KANKEN_KANJI_SUBDECK_ID,
+        "漢検一級::言葉"
+    )
+
+    for kanji in kanjis:
+        note = create_kanji_note(kanji)
+        kanken_kanji_subdeck.add_note(note)
+
+    for kotoba in kotobas:
+        note = create_kotoba_note(kotoba)
+        kanken_kotoba_subdeck.add_note(note)
+    
+    package = genanki.Package([kanken_kanji_subdeck, kanken_kotoba_subdeck])
+    return package
--- kanken_processor.py
+++ kanken_processor.py
@@ -1,26 +1,47 @@
+import os
 import sys
 import argparse
 from typing import Iterable
 
+from data_models import Kanji, Kotoba
+
+CACHE_OBJECT_PATH = "build/kanjipedia_cache.pickle"
+def parse_data_cached() -> tuple[Iterable[Kanji], Iterable[Kotoba]]:
+    """Use pickling to cache parsed kanji/kotoba data on disk and retrieve it if already stored.
+    If the data is over a certain age, it may be invalidated.
+    """
+    import pickle
+    try:
+        with open(CACHE_OBJECT_PATH, "rb") as f:
+            kanji, kotoba = pickle.load(f)
+            return kanji, kotoba
+    except FileNotFoundError:
+        from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
+        kanji, kotoba = parse_all_kanji(), parse_all_kotoba()
+        with open(CACHE_OBJECT_PATH, "wb") as f:
+            pickle.dump((list(kanji), list(kotoba)), f)
+
 def generate_anki_deck():
     from anki_deck_generator import build_deck
-    from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
+    # from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
     print("Building Anki deck...", file=sys.stderr)
-    package = build_deck(parse_all_kanji(), parse_all_kotoba())
+    # package = build_deck(parse_all_kanji(), parse_all_kotoba())
+    package = build_deck(*parse_data_cached())
     package.write_to_file("build/漢検一級.apkg")
 
 def generate_data_files():
-    from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
+    # from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
     print("Building static data files...", file=sys.stderr)
 
     def generate_dump(output_file: str, iterable: Iterable):
         with open(output_file, mode="w") as f:
             f.write("\n".join(str(item) for item in iterable))
 
-    all_kanji = parse_all_kanji()
+    # all_kanji = parse_all_kanji()
+    all_kanji, all_kotoba = parse_data_cached()
     generate_dump("build/kanji.tsv", all_kanji)
 
-    all_kotoba = parse_all_kotoba()
+    # all_kotoba = parse_all_kotoba()
     generate_dump("build/kotoba.tsv", all_kotoba)
 
 def main():
@@ -29,8 +50,13 @@ def main():
         description="Program that collates Kanken data",
     )
     cli_parser.add_argument("action", choices=["compile-data", "compile-deck", "compile-all"])
+    cli_parser.add_argument("--purge-cache", action="store_true", dest="purge_cache")
 
     args = cli_parser.parse_args()
+
+    # Remove the object storing the cache
+    if args.purge_cache:
+        os.remove(CACHE_OBJECT_PATH)
 
     action: str = args.action
     if action == "compile-data":
