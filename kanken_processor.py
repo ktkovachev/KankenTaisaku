19ee229663ec0dd7950a5de2ba531f12431126f1
Kiril Kovachev <ktkovachev@gmail.com>
2024-08-30 21:06:42 +0100

    Flesh out driver program (Anki deck, more generic code)

--- kanken_processor.py
+++ kanken_processor.py
@@ -1,21 +1,26 @@
 import sys
 import argparse
+from typing import Iterable
 
 def generate_anki_deck():
     from anki_deck_generator import build_deck
+    from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
     print("Building Anki deck...", file=sys.stderr)
-    pass
+    build_deck(parse_all_kanji(), parse_all_kotoba())
 
 def generate_data_files():
     from kanjipedia_collator import parse_all_kanji, parse_all_kotoba
+    print("Building static data files...", file=sys.stderr)
 
-    # all_kanji = parse_all_kanji()
-    # with open("build/kanji.tsv", mode="w") as f:
-    #     f.write("\n".join(str(kanji) for kanji in all_kanji))
+    def generate_dump(output_file: str, iterable: Iterable):
+        with open(output_file, mode="w") as f:
+            f.write("\n".join(str(item) for item in iterable))
+
+    all_kanji = parse_all_kanji()
+    generate_dump("build/kanji.tsv", all_kanji)
 
     all_kotoba = parse_all_kotoba()
-    with open("build/kotoba.tsv", mode="w") as f:
-        f.write("\n".join(str(kotoba) for kotoba in all_kotoba))
+    generate_dump("build/kotoba.tsv", all_kotoba)
 
 def main():
     cli_parser = argparse.ArgumentParser(
