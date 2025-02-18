c5c21ec2d9b3618a5e8e1737a779f9d589a445b4
Kiril Kovachev <ktkovachev@gmail.com>
2025-02-18 22:48:29 +0000

    Add phonetic series scraping script

---
+++ phonetic_series.py
@@ -0,0 +1,34 @@
+import pywikibot
+from tqdm import tqdm
+import json
+
+SITE = pywikibot.Site("en", "wiktionary")
+
+BaxterSagart = str
+ZhengzhangShangfang = str
+
+# Literal page sources
+def get_character_data_raw(char: str) -> tuple[BaxterSagart, ZhengzhangShangfang]:
+    return (
+        pywikibot.Page(SITE, f"Module:zh/data/och-pron-ZS/{char}").text,
+        pywikibot.Page(SITE, f"Module:zh/data/och-pron-BS/{char}").text
+    )
+
+with open("supplementary/pronunciation/phonetic_series/group.json") as f:
+    phonetic_series: dict[str, list[str]] = json.load(f)
+
+data_dict = {}
+i = 0
+for phonetic_component, characters in (bar := tqdm(phonetic_series.items())):
+    for character in characters:
+        bar.set_description(f"Getting {character}")
+        data_dict[character] = get_character_data_raw(character)
+    i += 1
+    if i == 3:
+        break
+
+# TODO: fix path / don't save at all? Or, use a dump.
+with open("old-chinese-data.json", "w") as f:
+    json.dump(data_dict, f, ensure_ascii=False, indent=4)
+
+# TODO: process data further
